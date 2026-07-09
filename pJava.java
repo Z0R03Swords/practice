57168b0e1c4563a5fb3af49eb94f15547bcaaced
Priyanshu Nimawat <priynimawat135@gmail.com>
2026-07-09 14:00:32 +0530

    upate the code after minor improvement

--- pJava.java
+++ pJava.java
@@ -49,7 +49,7 @@ public static int add(int a, int b){
 return a+b;
 }
 
-public statuc int sub(int a, int b){
+public static int sub(int a, int b){
 return a-b;
 }
 public static int mul(int a, int b){
