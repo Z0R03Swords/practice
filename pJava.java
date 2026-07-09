5cebb851c6e76ff021212dfa331acc46270cf1de
Priyanshu Nimawat <priynimawat135@gmail.com>
2026-07-09 13:29:27 +0530

    Added pJava.java and pJava.class

---
+++ pJava.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class pJava{
+
+public static void main(String args[]){
+
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Enterthe first number");
+int a = sc.nextInt();
+System.out.println("Enterthe second number");
+int b = sc.nextInt();
+
+System.out.println("Enterthe choice");
+
+int choice = sc.nextInt();
+
+switch(choice){
+
+case 1:
+
+int sum = add(a,b);
+System.out.println(sum); 
+break;
+
+default:
+
+System.out.println("enter the valid choice");
+
+}
+
+
+
+
+}
+
+
+public static int add(int a, int b){
+return a+b;
+}
+
+
+}
